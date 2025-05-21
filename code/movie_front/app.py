7b3f02d2b24290fa0bf938f5d4c364d10df29a71
AxelMAO <96473621+AxelMAO@users.noreply.github.com>
2025-05-21 21:33:22 +0200

    Part1 closed

--- code/movie_front/app.py
+++ code/movie_front/app.py
@@ -40,4 +40,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    #app.run(debug=True, port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
